485113b23bb749eef8481b96b4337ad537921380
firattamur <ftamur16@ku.edu.tr>
2023-03-21 15:53:35 +0300

    :wrench: chore: add docker files and spring boot project

---
+++ src/test/java/com/firattamur/dockerizedspringbootmicroservice/DockerizedSpringBootMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firattamur.dockerizedspringbootmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedSpringBootMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
